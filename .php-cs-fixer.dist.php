ebcf621ed4c0cdd94749d5997c1bb957cb2781f7
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2022-08-09 13:58:13 -0300

    Update PHPUnit and add more dev dependencies

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of coisa/http-client.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/http-client
+ * @copyright Copyright (c) 2022 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+$paths = [
+    __FILE__,
+    __DIR__,
+];
+
+$header = file_get_contents(__DIR__ . '/.docheader');
+
+return CoiSA\PhpCsFixer\PhpCsFixer::create($paths, (string) $header);
